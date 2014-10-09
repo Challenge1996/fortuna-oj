14ec09d88ff2dc9c344cf17f93440eb493d1d7d8
roastduck <beantang.tang@gmail.com>
2014-10-09 21:22:14 +0800

    show allowed languages in contest/home

--- application/views/contest/home.php
+++ application/views/contest/home.php
@@ -3,6 +3,7 @@
 	echo "<h2>$data->title</h2>";
 	echo "<p class=\"explanation\">Start Time: <span class=\"badge badge-info\">$data->startTime</span> " . 
 		 "Submit Time: <span class=\"badge badge-info\">$data->submitTime</span> " .
-		 "End Time: <span class=\"badge badge-info\">$data->endTime</span> Status: $data->status</p>";
+		 "End Time: <span class=\"badge badge-info\">$data->endTime</span> Status: $data->status<br />" .
+		 "Allowed Languages: <font color='#006652'>$data->language</font></p>";
 	echo "<p>$data->description</p>";
 	echo '</div>';
