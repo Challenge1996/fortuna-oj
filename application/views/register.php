9a2ebe8eb6e2eab2692faf7529673f41d9353e66
roastduck <beantang.tang@gmail.com>
2015-06-23 11:52:02 +0800

    modify register UI

--- application/views/register.php
+++ application/views/register.php
@@ -2,71 +2,69 @@
 	$data = array(
 		'username' => array(
 			'name' => 'username',
+			'id' => 'username',
 			'value' => set_value('username'),
 			'placeholder' => 'Username*',
-			'style' => 'margin-left:160px'
 		),
 		'password' => array(
 			'name' => 'password',
+			'id' => 'password',
 			'placeholder' => 'Password*',
-			'style' => 'margin-left:160px'
 		),
 		'confirm_password' => array(
 			'name' => 'confirm_password',
+			'id' => 'confirm_password',
 			'placeholder' => 'Confirm Password*',
-			'style' => 'margin-left:160px'
 		),
 		'email' => array(
 			'name' => 'email',
+			'id' => 'email',
 			'value' => set_value('email'),
 			'placeholder' => 'Email Address*',
-			'style' => 'margin-left:160px'
 		),
 		'school' => array(
 			'name' => 'school',
+			'id' => 'school',
 			'value' => set_value('school'),
 			'placeholder' => 'School',
-			'class' => 'span6',
-			'style' => 'margin-left:160px'
 		),
 		'description' => array(
 			'name' => 'description',
+			'id' => 'description',
 			'value' => set_value('description'),
 			'placeholder' => 'Descrption',
-			'class' => 'span6',
-			'style' => 'margin-left:160px'
 		)
 	);
 ?>
 	
-<div id="register_field" class="modal" style="width:740px; margin-left:-370px">
+<div id="register_field" class="modal">
 	<div class="modal-header"><h3><em>Register</em></h3></div>
 	
 	<form action="index.php/main/register" id="register_form" class="form" method="post">
-		<div class="modal-body">
-			<div>
-				<span style="position:absolute; left:10"><i class="icon-user"></i>Username*</span>
-				<?=form_input($data['username']) . form_error('username')?>
+		<div class="modal-body form-horizontal">
+			<div class="control-group">
+				<label class="control-label" for="username"><i class="icon-user"></i>Username*</label>
+				<div class="controls"><?=form_input($data['username']) . form_error('username')?></div>
 			</div>
-			<div>
-				<span style="position:absolute; left:10"><i class="icon-envelope"></i>Email Address*</span>
-				<?=form_input($data['email']) . form_error('email') . '<br />'?>
+			<div class="control-group">
+				<label class="control-label" for="email"><i class="icon-envelope"></i>Email Address*</label>
+				<div class="controls"><?=form_input($data['email']) . form_error('email')?></div>
 			</div>
-			<div>
-				<span style="position:absolute; left:10"><i class="icon-briefcase"></i>Password*</span>
-				<?=form_password($data['password']) . form_error('password')?>
+			<div class="control-group">
+				<label class="control-label" for="password"><i class="icon-briefcase"></i>Password*</label>
+				<div class="controls"><?=form_password($data['password']) . form_error('password')?></div>
 			</div>
-			<div>
-				<span style="position:absolute; left:10"><i class="icon-repeat"></i>Confirm Password*</span>
-				<?=form_password($data['confirm_password']) . form_error('confirm_password')?>
+			<div class="control-group">
+				<label class="control-label" for="confirm_password"><i class="icon-repeat"></i>Confirm Password*</label>
+				<div class="controls"><?=form_password($data['confirm_password']) . form_error('confirm_password')?></div>
 			</div>
-			<div>
-				<span style="position:absolute; left:10"><i class="icon-home"></i>School</span>
-				<?=form_input($data['school']) . '<br />'?>
+			<div class="control-group">
+				<label class="control-label" for="confirm_password"><i class="icon-home"></i>School</label>
+				<div class="controls"><?=form_input($data['school']) . '<br />'?></div>
 			</div>
-			<div>
-				<span style="position:absolute; left:10"><i class="icon-star"></i>Descrption</span>
-				<?=form_textarea($data['description'])?>
+			<div class="control-group">
+				<label class="control-label" for="description"><i class="icon-star"></i>Descrption</label>
+				<div class="controls"><?=form_textarea($data['description'])?></div>
 			</div>
 		</div>
 		
