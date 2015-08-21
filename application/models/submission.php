1d3af56b0551d17c80a079a78ebddb8361a3e939
Yang Jiaqi <guru@live.hk>
2015-08-21 19:56:39 +0800

    emergency fix

--- application/models/submission.php
+++ application/models/submission.php
@@ -168,14 +168,15 @@ class Submission extends CI_Model{
 	function allow_view_code($sid)
 	{
 		$this->load->model('problems');
+		$this->load->model('contests');
 		$result = $this->db->query("SELECT uid, pid, cid, private FROM Submission WHERE sid=?", array($sid));
 		if ($result->num_rows() == 0) return FALSE; else $result = $result->row();
 		$uid = $this->session->userdata('uid');
-		if (isset($result->cid)) $contest_status = $this->contest->load_contest_status($result->cid);
+		if (isset($result->cid)) $contest_status = $this->contests->load_contest_status($result->cid);
 		if (!(isset($contest_status) && $contest_status->running) && !$this->problems->allow($result->pid)) return FALSE;
 		$accepted = $this->db->query("SELECT * FROM Submission WHERE pid=? AND uid=? AND status=0", array($result->pid, $uid))->num_rows() > 0;
 		if ($this->db->query("SELECT pid FROM ProblemSet WHERE pid=? AND isShowed=1", array($result->pid))->num_rows() == 0) $accepted = FALSE;
-		if ($contest_status->running) $accepted = FALSE;
+		if (isset($contest_status) && $contest_status->running) $accepted = FALSE;
 		if ($result->uid != $uid && !$this->user->is_admin() && $result->private != 0 && !$accepted)
 			return FALSE;
 		return TRUE;
