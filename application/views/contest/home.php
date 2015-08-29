4abb21bfb5ddb4c9ecfba36db2d1046416de4bc9
Yang Jiaqi <guru@live.hk>
2015-08-29 21:15:56 +0800

    show duration time for `template contest`

--- application/views/contest/home.php
+++ application/views/contest/home.php
@@ -5,20 +5,21 @@
 		Submit Time: <span class="badge badge-info"><?=$data->submitTime?></span>
 		End Time: <span class="badge badge-info"><?=$data->endTime?></span> Status: <?=$data->status?><br />
 	</p>
-	<?php if ($data->isTemplate == 1): ?>
-	<?php if (!$this->user->is_admin()): ?>
+	<?php if ($data->isTemplate == 1 && !$this->user->is_admin()): ?>
 	<p class="act_time">
 		<?php if (isset($custom->startTime)): ?>
 		Your Start Time: <span class="badge badge-info"><?=$custom->startTime?></span>
 		Your Submit Time: <span class="badge badge-info"><?=$custom->submitTime?></span>
 		Your End Time: <span class="badge badge-info"><?=$custom->endTime?></span>
 		Your Status: <?=$custom->status?><br />
-		<?php elseif (!$this->user->is_admin() &&
-				strtotime('now') + strtotime($this->contests->load_relative_time($data->cid)->endTime) <= strtotime($data->endTime)): ?>
+		<?php else: $temp = $this->contests->load_relative_time($data->cid); ?>
+		Submit After: <span class="badge badge-info"><?=gmdate("H:i", $temp->submitTime)?></span>
+		End After: <span class="badge badge-info"><?=gmdate("H:i", $temp->endTime)?></span>
+		<?php if (strtotime('now') + $temp->endTime <= strtotime($data->endTime)): ?>
 		<button class="btn btn-primary" onclick="start_contest_onclick()">Start</button>
 		<?php endif; ?>
+		<?php endif; ?>
 	</p>
-	<?php endif; ?>
 	<?php endif; ?>
 	<p class="allowed_languages">Allowed Languages: <font color='#006652'><?=$data->language?></font></p>
 	<p><?=$data->description?></p>
