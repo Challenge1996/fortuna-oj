de624daba3797b7bbbe93cc7fc16140ec4633d7f
mchobbylong <mchobbylong@gmail.com>
2019-02-16 21:40:58 +0800

    style: no underlines on hrefs in userinfo

--- application/views/userinfo.php
+++ application/views/userinfo.php
@@ -11,14 +11,14 @@
 			</div>
 		</a>
 		<div style='text-align:center; margin-top:3px'>
-			<a href='#misc/mailbox'>
+			<a href='#misc/mailbox' style="text-decoration: none">
 				<i class='icon-envelope'></i>
 				<span class='badge badge-important' id="unread_mail_count" style="padding: 1px 4px"></span>
 			</a>
-			<a id="setting" href="#users/<?=$user?>/settings">
+			<a id="setting" href="#users/<?=$user?>/settings" style="text-decoration: none">
 				<i class="icon-cog" style="margin-left: 3px"></i>
 			</a>
-			<a id="logout" href="javascript:void(0)">
+			<a id="logout" href="javascript:void(0)" style="text-decoration: none">
 				<i class="icon-off" style="margin-left: 3px"></i>
 			</a>
 		</div>
