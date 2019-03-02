d83c655cf9a84445a4d819b401c6ef6bf4f50c8a
Michael Long <mchobbylong@gmail.com>
2019-03-02 20:29:34 +0800

    style: remove payid and adjust column order

--- application/views/admin/pay_orders.php
+++ application/views/admin/pay_orders.php
@@ -3,8 +3,8 @@
 <table class="table table-bordered table-condensed table-stripped">
 	<thead style="background-color:#89cff0">
 		<?php foreach (array(
+			'status' => '',
 			'orderid' => '#',
-			'payid' => lang('payapi_id'),
 			'uid' => 'UID',
 			'name' => lang('username'),
 			'itemDescription' => lang('item_description'),
@@ -12,7 +12,6 @@
 			'price' => lang('price'),
 			'realPrice' => lang('realprice'),
 			'method' => lang('pay_method'),
-			'status' => lang('status'),
 			'createTime' => lang('create_time'),
 			'finishTime' => lang('finish_time')
 			) as $key => $title): ?>
@@ -30,14 +29,20 @@
 	</thead>
 	<tbody><?php
 		foreach ($data as $row){
-			if ($row->status == 1)
+			if ($row->status == 1){
 				echo "<tr style='background-color:#6DEF9D'>";
-			else if ($row->status == -1)
+				$iconType = 'iconfont iconfont-yiwancheng1';
+			}
+			else if ($row->status == -1){
 				echo "<tr style='background-color:#FF8888'>";
-			else
+				$iconType = 'icon-remove';
+			}
+			else {
 				echo "<tr>";
+				$iconType = 'iconfont iconfont-dengdai';
+			}
+			echo "<td><i class='$iconType'></i></td>";
 			echo "<td>$row->orderid</td>";
-			echo "<td>$row->payid</td>";
 			echo "<td>$row->uid</td>";
 			echo "<td><span class='label label-info'><a href='#users/$row->name'>$row->name</a></span></td>";
 			echo "<td>$row->itemDescription</td>";
@@ -50,10 +55,6 @@
 				echo "<td><i class='iconfont iconfont-umidd17'></i></td>";
 			else 
 				echo "<td><i class='iconfont iconfont-pay-wechat'></i></td>";
-			if ($row->status == 0) $iconType = 'iconfont iconfont-dengdai';
-			else if ($row->status == 1) $iconType = 'iconfont iconfont-yiwancheng1';
-			else $iconType = 'icon-remove';
-			echo "<td><i class='$iconType'></i></td>";
 			echo "<td>$row->createTime</td>";
 			echo "<td>$row->finishTime</td></tr>";
 		}
